24c60791c98f3f99df80c02659213bf7dad1c5a2
megh <meghanas912@gmail.com>
2021-10-29 21:58:58 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,25 +1,127 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Modal from "react-modal";
+import { useState, useEffect } from "react";
 
 function App() {
+
+  const [users, setUser] = useState([]);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  
+
+  const getUser = async () => {
+    const response = await fetch("https://reqres.in/api/users/");
+    const json = await response.json();
+    setUser(json.data);
+  };
+
+
+  
+
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2 className="heading">Welcome to the list of Users</h2>
+      <div className="container-fluid mt-5">
+        <div className="row text-center">
+
+        
+          {users.length &&
+            users.map((item) => {
+              console.log(item)
+              return (
+                <div className="col-10 col-md-10 mt-5" key={item.id}>
+                  <div className="card p-2">
+                    <div className="d-flex align-items-center">
+                      <div className="image">
+                        {" "}
+                        <img
+                          src={item.avatar}
+                          className="rounded"
+                          width="155"
+                        />{" "}
+                      </div>
+
+                      <div className="p-2 mt-2  bg-primary d-flex justify-content-between rounded text-white stats">
+                        <div className="d-flex flex-column">
+                          <strong>
+                            {item.first_name} {item.last_name}
+                          </strong>
+                        </div>
+                      </div>
+
+                      <div className="d-flex flex-column">
+                        <button
+                          type="button"
+                          onClick={() => setModalIsOpen(true)}
+                          class="btn btn-secondary  btn-sm"
+                        >
+                          Know More
+                        </button>
+                        
+                        <Modal
+                        
+                          isOpen={modalIsOpen}
+                          shouldcloseonOverlay={false}
+                          ariaHideApp={false}
+                          onRequestClose={() => setModalIsOpen(false)}
+                        >
+                        <div className="flex">
+                        <table align="center">
+                        <tr>
+                        <td>Id</td>
+ <td>Fullname</td>
+ <td>Email</td>
+ 
+ </tr>
+ <tr>
+   <td>{item.id}</td>
+   <td>{item.first_name}{item.last_name}</td>
+   <td>{item.email}</td>
+   <td><div className="d-flex flex-column">
+                      <div className="image">
+                        {" "}
+                        <img
+                          src={item.avatar}
+                          className="rounded"
+                          width="155"
+                        />{" "}
+                      </div>
+                      </div></td>
+   
+ </tr>
+ </table>
+                     
+       <div>
+       <button  className="mbtn" onClick={()=>setModalIsOpen(false)}>Close</button>
+       </div>
+      </div>
+                          
+                        </Modal>
+
+
+
+
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+        </div>
+      </div>
+     
     </div>
+   
+   
+    
+    
   );
 }
-
 export default App;
+
+
--- src/index.js
+++ src/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+
+//import '/node_modules/bootstrap/dist/css/bootstrap.min.css';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+   
   </React.StrictMode>,
   document.getElementById('root')
 );
